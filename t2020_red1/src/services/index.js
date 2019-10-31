ae75fa819e049d6b3d3649ae450343b2e74e7216
Juun Tang <31942987+16SIC063U@users.noreply.github.com>
2019-10-31 14:17:40 +0800

    Update Services

--- t2020_red1/src/services/index.js
+++ t2020_red1/src/services/index.js
@@ -1,71 +1,261 @@
-const var IDENTITY = 'Group27';
-const var TOKEN = '895784ca-f09d-4242-a523-4beff6fa4221';
+const IDENTITY = 'Group27';
+const TOKEN = '895784ca-f09d-4242-a523-4beff6fa4221';
 
 // Customer
 // Get Customer ID
-export const getCustomerID(username) {
+export const getCustomerID = (username, callback) => {
+	//Regex Validation
+	if(!username.match(/.+/)){
+		return {OK: false}
+	}
+
 	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/"+username;
 	fetch(url,{
 	    method: 'GET',
 	    headers: {
 	        method: 'GET',
-	        identity: IDENTITY
+	        identity: IDENTITY,
 	        token: TOKEN,
 	        'mode': 'no-cors',
 	        'Access-Control-Allow-Origin': "*",
 	    }
 	})
 	.then((res) => {
-	    return res.json()
+	    return res.json();
 	})
 	.then((resJson) => {
-	    console.log("Response", resJson.customerId);
+	    callback(resJson);
 	});
 }
 
-// Get Customer Details
-export const getCustomerDetails(){
 
+// Get Customer Details
+export const getCustomerDetails = (customerId, callback) => {
+	//Regex Validation
+	if(!customerId.match(/.+/)){
+		return {OK: false}
+	}
+
+	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/"+customerId+"/details";
+	fetch(url,{
+	    method: 'GET',
+	    headers: {
+	        method: 'GET',
+	        identity: IDENTITY,
+	        token: TOKEN,
+	        'mode': 'no-cors',
+	        'Access-Control-Allow-Origin': "*",
+	    }
+	})
+	.then((res) => {
+	    return res.json();
+	})
+	.then((resJson) => {
+	   	callback(resJson);
+	});
 }
 
 // Transactions
 // Get Transaction Details
-export const getTransactionDetails(){
+export const getTransactionDetails = (accountId, fromDate, toDate, callback) => {
+	//Regex Validation
+	if(!accountId.match(/.+/)){
+		return {OK: false}
+	}
 
+	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/transactions/"+accountId+"?from="+fromDate+"&to="+toDate;
+	fetch(url,{
+	    method: 'GET',
+	    headers: {
+	        method: 'GET',
+	        identity: IDENTITY,
+	        token: TOKEN,
+	        'mode': 'no-cors',
+	        'Access-Control-Allow-Origin': "*",
+	    }
+	})
+	.then((res) => {
+	    return res.json();
+	})
+	.then((resJson) => {
+	   	callback(resJson);
+	});
 }
 
 // Accounts
 // Get List of Deposit Accounts
-export const getDepositAccounts(){
+export const getDepositAccounts = (customerId, callback) => {
+	//Regex Validation
+	if(!customerId.match(/.+/)){
+		return {OK: false}
+	}
 
+	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/"+customerId;
+	fetch(url,{
+	    method: 'GET',
+	    headers: {
+	        method: 'GET',
+	        identity: IDENTITY,
+	        token: TOKEN,
+	        'mode': 'no-cors',
+	        'Access-Control-Allow-Origin': "*",
+	    }
+	})
+	.then((res) => {
+	    return res.json();
+	})
+	.then((resJson) => {
+	   	callback(resJson);
+	});
 }
 // Get Balance of a Deposit Account
-export const getDepositAccBalance(){
+export const getDepositAccBalance = (accountId, month=0, year=2018, callback) => {
+	//Regex Validation
+	if(!accountId.match(/.+/)){
+		return {OK: false}
+	}
 
+	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/"+accountId+"/balance?month="+month+"&year="+year;
+	fetch(url,{
+	    method: 'GET',
+	    headers: {
+	        method: 'GET',
+	        identity: IDENTITY,
+	        token: TOKEN,
+	        'mode': 'no-cors',
+	        'Access-Control-Allow-Origin': "*",
+	    }
+	})
+	.then((res) => {
+	    return res.json();
+	})
+	.then((resJson) => {
+	   	callback(resJson);
+	});
 }
 // Get List of Credit Accounts
-export const getCreditAccounts(){
+export const getCreditAccounts = (customerId, callback) => {
+	//Regex Validation
+	if(!customerId.match(/.+/)){
+		return {OK: false}
+	}
 
+	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/credit/"+customerId;
+	fetch(url,{
+	    method: 'GET',
+	    headers: {
+	        method: 'GET',
+	        identity: IDENTITY,
+	        token: TOKEN,
+	        'mode': 'no-cors',
+	        'Access-Control-Allow-Origin': "*",
+	    }
+	})
+	.then((res) => {
+	    return res.json();
+	})
+	.then((resJson) => {
+	   	callback(resJson);
+	});
 }
 
 // Get Outstanding Balance of a Credit Account
-export const getCreditAccBalance(){
-	
+export const getCreditAccBalance = (accountId, callback) => {
+	//Regex Validation
+	if(!accountId.match(/.+/)){
+		return {OK: false}
+	}
+
+	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/credit/"+accountId+"/balance";
+	fetch(url,{
+	    method: 'GET',
+	    headers: {
+	        method: 'GET',
+	        identity: IDENTITY,
+	        token: TOKEN,
+	        'mode': 'no-cors',
+	        'Access-Control-Allow-Origin': "*",
+	    }
+	})
+	.then((res) => {
+	    return res.json();
+	})
+	.then((resJson) => {
+	   	callback(resJson);
+	});
 }
 
 // Marketing
 // Get List of Marketing Messages
-export const getMarketingMessages(){
-	
+export const getMarketingMessages = (callback) => {
+	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/marketing";
+	fetch(url,{
+	    method: 'GET',
+	    headers: {
+	        method: 'GET',
+	        identity: IDENTITY,
+	        token: TOKEN,
+	        'mode': 'no-cors',
+	        'Access-Control-Allow-Origin': "*",
+	    }
+	})
+	.then((res) => {
+	    return res.json();
+	})
+	.then((resJson) => {
+	   	callback(resJson);
+	});
 }
 
 // Get Details of a Marketing Message
-export const getMessageDetails(){
-	
+export const getMessageDetails = (id, callback) => {
+	//Regex Validation
+	if(!id.match(/.+/g)){
+		return {err: true}
+	}
+
+	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/marketing/"+id;
+	fetch(url,{
+	    method: 'GET',
+	    headers: {
+	        method: 'GET',
+	        identity: IDENTITY,
+	        token: TOKEN,
+	        'mode': 'no-cors',
+	        'Access-Control-Allow-Origin': "*",
+	    }
+	})
+	.then((res) => {
+	    return res.json();
+	})
+	.then((resJson) => {
+	   	callback(resJson);
+	});
 }
 
 // Message
 // Get Personal Messages
-export const getPersonalMessages(){
-	
+export const getPersonalMessages = (id, callback) => {
+	//Regex Validation
+	if(!id.match(/.+/)){
+		return {OK: false}
+	}
+
+	let  url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/message/"+id;
+	fetch(url,{
+	    method: 'GET',
+	    headers: {
+	        method: 'GET',
+	        identity: IDENTITY,
+	        token: TOKEN,
+	        'mode': 'no-cors',
+	        'Access-Control-Allow-Origin': "*",
+	    }
+	})
+	.then((res) => {
+	    return res.json();
+	})
+	.then((resJson) => {
+	   	callback(resJson);
+	});
 }
